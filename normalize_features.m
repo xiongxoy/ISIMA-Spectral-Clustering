cde304de8fec47b162d24d4259156c106467707d
xiongxoy <xiong.xoy@gmail.com>
2013-05-22 17:37:02 +0200

    Correct normalize_features.m

--- normalize_features.m
+++ normalize_features.m
@@ -1,26 +1,22 @@
 function F_ = normalize_features(F)
 %% normalize to gaussian
-  d = size(F{1});
+  d = max(size(F{1}));
   [p q] = size(F);
 
-  S = ones(p*q, d);
-  for i = 1:p
-    for j = 1:q
-      S( (i-1)*q + i ) = F{i, j};
-    end
+  S = zeros(p*q, d);
+  for i = 1:p*q
+    S(i, :) = F{i};
   end
 
   S_mean = mean(S);
   S_sigma = sqrt(var(S));
-  for i = 1:p
+  for i = 1:p*q
     S(i, :) = (S(i, :) - S_mean) ./ S_sigma;
   end
 
-  for i = 1:p
-    for j = 1:q
-      F{i, j} = S( (i-1)*q + i , :);
-    end
+  for i = 1:p*q
+    F{i} = S(i, :);
   end
 
-  F_ = F;
+  F_ = reshape(F, size(F));
 end
