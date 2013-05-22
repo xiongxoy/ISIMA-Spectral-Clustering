8bdb9dbe96c5a0f746933d5db7b6a6c13cfe9b53
xiongxoy <xiong.xoy@gmail.com>
2013-05-22 17:36:23 +0200

    Update feature_extractor.m

--- feature_extractor.m
+++ feature_extractor.m
@@ -1,24 +1,36 @@
 function F = feature_extractor(I)
-% This function extracts features specified by @a type from input image
+% This function extracts features from inpute image
 % @param I inpute image
 % @return F a cell matrix specifying feature vetors for each pixel
   F = cell(size(I));
-  F = append_F(F, I);
-  %F = append_F(F, get_pos_X(I));
-  %F = append_F(F, get_pos_Y(I));
+  F = append_F(F, double(I));
+  F = append_F(F, get_pos_X(I));
+  F = append_F(F, get_pos_Y(I));
+  %F = append_F(F, get_Partial_X(I));
+  %F = append_F(F, get_Partial_Y(I));
   %F = append_F(F, get_Variance(I));
-  %F = append_F(F, get_Mean(I));
+  F = append_F(F, get_Mean(I));
+  %F = append_F(F, texture_analyser(I));
+  F = normalize_features(F);
+
   if isempty(F)
     warning('No Feature is Extracted');
   end
 end
 
 function F_ = append_F(F, S)
-p = size(F,1);
-q = size(F,2);
-for i = 1:p
-  for j = 1:q
-    F{i,j}(end+1) = S(i,j);
+ p = size(F,1);
+ q = size(F,2);
+if iscell(S) == 1
+  d = max(size(S{1}));
+  for i = 1:p*q
+    for j = 1:d
+      F{i}(end+1) = S{i}(j);
+    end
+  end
+else
+  for i = 1:p*q
+    F{i}(end+1) = S(i);
   end
 end
 F_ = F;
@@ -74,14 +86,10 @@ end
 function S = get_Variance(I)
   [p q] = size(I);
   S = zeros(size(I));
-  for i = 1:p
-    for j = 1:q
-      try
+  for i = 2:p-1
+    for j = 2:q-1
         patch = I(i-1:i+1, j-1:j+1);
-        S(i, j) = var(patch(:));
-      catch Error_Msg
-        S(i, j) = 0; % TODO Find a better way?
-      end
+        S(i, j) = var(double(patch(:)));
     end
   end
 end
